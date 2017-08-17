7115a31f8d014e63fa150bddc97c7927b5e2a491
Glen Vermeylen <glen.vermeylen@gmail.com>
2017-08-17 07:35:16 +0200

    play around with messagebus

--- src/main/java/be/glever/anttest/MessageBusTest_Main.java
+++ src/main/java/be/glever/anttest/MessageBusTest_Main.java
@@ -9,29 +9,63 @@ import java.util.stream.StreamSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import be.glever.anttest.MessageBusTest_Main.QueueElement;
+
 public class MessageBusTest_Main {
 	private static final Logger LOG = LoggerFactory.getLogger(MessageBusTest_Main.class);
 
 	public static void main(String[] args) {
 		LinkedBlockingQueue<QueueElement<Integer>> bq = new LinkedBlockingQueue<>();
+
+		MessageProducer messageProducer = new MessageProducer(bq);
+		Thread producer = new Thread(messageProducer);
+		producer.start();
+
+		Stream<Integer> readStream = StreamSupport.stream(new MySpliterator<>(bq), false);
+		new Thread(() -> {
+			readStream.forEach(item -> LOG.info("" + item));
+		}).start();
 		
-		new Thread(()-> {
-			for(int i = 0 ; i < 10; i ++) {
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			Thread.interrupted();
+		}
+
+		messageProducer.stop();
+		producer.interrupt();
+
+	}
+
+	private static final class MessageProducer implements Runnable {
+		private final LinkedBlockingQueue<QueueElement<Integer>> bq;
+		private boolean stop;
+
+		private MessageProducer(LinkedBlockingQueue<QueueElement<Integer>> bq) {
+			this.bq = bq;
+		}
+		
+		public void stop() {
+			this.stop = true;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; ; i++) {
 				bq.add(new QueueElement<Integer>(i, false));
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					Thread.interrupted();
+					if(stop) {
+						LOG.info("asked to stop processing");
+						bq.add(new QueueElement<Integer>(-1, true));
+						break;
+					}
 				}
 			}
-			bq.add(new QueueElement<Integer>(-1, true));
-			
-		}).start();
-		
-		Stream<QueueElement<Integer>> readStream = StreamSupport.stream(new MySpliterator<>(bq), false);
-		new Thread(()-> {
-			readStream.forEach(item -> LOG.info("" + item.t));
-		}).start();
+
+		}
 	}
 
 	public static class QueueElement<T> {
@@ -42,31 +76,43 @@ public class MessageBusTest_Main {
 			this.t = t;
 			this.poison = poison;
 		}
+
+		public T get() {
+			return t;
+		}
+
+		public boolean isPoison() {
+			return poison;
+		}
+
 	}
-	
-	public static class MySpliterator<T> implements Spliterator<QueueElement<T>>{
+
+	public static class MySpliterator<T> implements Spliterator<T> {
 		private LinkedBlockingQueue<QueueElement<T>> queue;
 
 		public MySpliterator(LinkedBlockingQueue<QueueElement<T>> queue) {
-			this.queue = queue;			
+			this.queue = queue;
 		}
 
 		@Override
-		public boolean tryAdvance(Consumer<? super QueueElement<T>> action) {
+		public boolean tryAdvance(Consumer<? super T> action) {
 			try {
 				QueueElement<T> t = this.queue.take();
-				if(! t.poison) {
-					action.accept(t);
-					return true;					
+				if (!t.isPoison()) {
+					action.accept(t.get());
+					return true;
+				}else {
+					LOG.info("received poison message, stopping");
 				}
 			} catch (InterruptedException e) {
 				Thread.interrupted();
+				LOG.info("Interrupted, only reason would be to stop consuming");
 			}
 			return false;
 		}
 
 		@Override
-		public Spliterator<QueueElement<T>> trySplit() {
+		public Spliterator<T> trySplit() {
 			return null;
 		}
 
@@ -80,8 +126,6 @@ public class MessageBusTest_Main {
 			return Spliterator.ORDERED;
 		}
 
-	
-		
 	}
 
 }
